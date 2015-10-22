fa66c9efec712d85881d10ffba1b1e7fda6a1bca
Webb Henderson <webb.henderson@gmail.com>
2015-10-22 11:21:20 -0600

    fixed typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user =User.new(user_params)
+  	@user = User.new(user_params)
   	if @user.save
   		# Handle a successful save
   	else
