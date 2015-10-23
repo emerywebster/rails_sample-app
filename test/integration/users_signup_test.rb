ca0f5edde629f6cf74656f4e0448ddb46d8545ad
Webb Henderson <webb.henderson@gmail.com>
2015-10-23 13:37:47 -0600

    test for success flash upon successful signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,5 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
