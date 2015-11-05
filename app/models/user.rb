d8c8ac0e28fb6b56115a57e4eccf5232197748b7
Webb Henderson <webb.henderson@gmail.com>
2015-11-04 17:21:50 -0700

    define new token and digest methods using class << self

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,16 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  class << self
   # Returns the hash digest of the given string.
-  def self.digest(string)
+  def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
-  def self.new_token
+  def new_token
     SecureRandom.urlsafe_base64
   end
 
